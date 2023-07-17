8f1d21f3f5a35e58d10f1faf8253464bd94cac77
HectorB <elmillo1509@gmail.com>
2023-07-17 19:32:14 -0300

    lista con tabla finalizada

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, AfterContentInit, AfterViewInit } from '@angular/core';
+import { MatTable, MatTableDataSource } from '@angular/material/table';
 
+export interface Item {
+  nombre: string;
+  costo: string;
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'proyectoEntrega';
+export class AppComponent implements AfterViewInit{
+  listaItems = [
+    { nombre: 'Platos', costo: '10' },
+    { nombre: 'Vasos', costo: '20' },
+    { nombre: 'Cubiertos', costo: '30' },
+    { nombre: 'Cervezas', costo: '40' },
+    { nombre: 'Gaseosas', costo: '50' },
+    { nombre: 'Galletas', costo: '60' },
+  ];
+  mostrarLista: boolean;
+
+  constructor() {
+    this.mostrarLista = false;
+  }
+
+
+  columnas: string[] = ['nombre', 'costo'];
+  dataSource = new MatTableDataSource<Item>(this.listaItems);
+
+  @ViewChild(MatTableDataSource) table!: MatTableDataSource<Item>;
+
+  ngAfterViewInit() {
+    this.dataSource.data = this.listaItems;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonModule} from '@angular/material/button';
+import { MatTableModule } from '@angular/material/table';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatTableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
